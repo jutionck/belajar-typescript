16e5c9963baa23ae1bfa6432f1dcfe873182534e
mipdevp code <mipdevp@gmail.com>
2021-06-07 10:32:55 +0700

    Example pass by value and pass by reference

---
+++ pass-reference.ts
@@ -0,0 +1,15 @@
+function callByReference(varObj: any) {
+  console.log("Inside Call by Reference Method");
+  varObj.a = 100;
+  console.log(varObj);
+}
+
+let varObj = { a: 1 };
+console.log("Before Call by Reference Method");
+console.log(varObj);
+
+callByReference(varObj)
+console.log("After Call by Reference Method");
+
+
+console.log(varObj);
